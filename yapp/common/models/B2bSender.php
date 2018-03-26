018eb46181e0359d6b155984b6d5d000b658a625
Quadrosh <quadrosh@gmail.com>
2018-03-26 16:57:03 +0300

    gearman yii 0.5

--- yapp/common/models/B2bSender.php
+++ yapp/common/models/B2bSender.php
@@ -186,6 +186,7 @@ class B2bSender extends Model
             ]
         ]));
 
+        return 'sent to gearman';
 //        $client = new \GearmanClient();
 //        $client->addServer();
 //        $client->setTimeout(29000);
