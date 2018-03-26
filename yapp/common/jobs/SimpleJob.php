2f689555120c09c4be289c057c3322e1b41952c0
Quadrosh <quadrosh@gmail.com>
2018-03-26 18:52:29 +0300

    gearman yii 0.10

--- yapp/common/jobs/SimpleJob.php
+++ yapp/common/jobs/SimpleJob.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace common\jobs;
+use common\models\B2bSender;
 use Yii;
 
 
@@ -13,7 +14,7 @@ class SimpleJob extends JobBase
         // Do something
 //        $content = $job->workload();
 
-        return 'there job done too';
+//        return 'there job done too';
 
         $info = [
             'action'=>'Gearman job',
@@ -27,44 +28,14 @@ class SimpleJob extends JobBase
         ];
         $chat_id = $options['chat_id'];
         $urlEncodedText = urlencode($options['text']);
-        $this->sendToUser('https://api.telegram.org/bot' .
+
+        $sender = new B2bSender();
+        $sender->sendToUser('https://api.telegram.org/bot' .
             'https://api.telegram.org/bot' .
             Yii::$app->params['b2bBotToken'].
             '/sendMessage?chat_id='.$chat_id .
             '&text='.$urlEncodedText, $options, true);
-
     }
 
-    public static function sendToUser($url, $options = [], $dataInBody = false)
-    {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Telebot');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        if (count($options)) {
-            curl_setopt($ch, CURLOPT_POST, true);
-            if ($dataInBody) {
-                $bodyOptions = $options;
-                unset($bodyOptions['chat_id']);
-                unset($bodyOptions['text']);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $bodyOptions);
-            }
-        }
-        $r = curl_exec($ch);
-        if($r == false){
-            $text = 'Gearman curl error '.curl_error($ch);
-            Yii::info($text, 'b2bBot');
-        } else {
-            $info = curl_getinfo($ch);
-            $info['url'] = str_replace(Yii::$app->params['b2bBotToken'],'_not_logged_',  $info['url']);
-            $info = [
-                    'action'=>'Gearman curl to User',
-                    'options'=>$options,
-                    'dataInBody'=>$dataInBody,
-                ] + $info;
-            Yii::info($info, 'b2bBot');
-        }
-        curl_close($ch);
-        return $r;
-    }
+
 }
