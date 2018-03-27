b1fb8811af9ea0cdd04c40c0337d910949167054
Quadrosh <quadrosh@gmail.com>
2018-03-27 20:21:16 +0300

    gearman yii 0.47

--- yapp/common/jobs/SimpleJob.php
+++ yapp/common/jobs/SimpleJob.php
@@ -18,17 +18,8 @@ class SimpleJob extends JobBase
         $jobWorkload = unserialize($workload);
         $options = $jobWorkload->params['options'];
 
-//        $jw = new JobWorkload;
-//        $opt = $jw->load()
-//        $par1 = $unserialWorkload->params;
-//        $par2 = $unserialWorkload['params'];
-//        $optionsFrom = unserialize($workload['options']);
-//        $workload2 = $job->workload(['options']);
 
-//        $options = [
-//            'chat_id' => '232544919',
-//            'text' => 'чек чек gearman',
-//        ];
+
         $chat_id = $options['chat_id'];
         $urlEncodedText = urlencode($options['text']);
 
@@ -42,15 +33,13 @@ class SimpleJob extends JobBase
         $info = [
             'action'=>'B2B Gearman Job send 2 user',
             'options'=>$options,
-//            'workload'=>$workload,
-//            'jobWorkload'=>$jobWorkload,
-//            '$options'=>$options,
+
         ];
         file_put_contents(dirname(dirname(__DIR__)).'/frontend/runtime/logs/job.log',
             '----------------'.PHP_EOL
             .date(" g:i a, F j, Y").PHP_EOL.print_r($info,true).PHP_EOL, FILE_APPEND);
 
-        return 'true';
+        return $result;
     }
 
 
--- yapp/frontend/controllers/B2bBotController.php
+++ yapp/frontend/controllers/B2bBotController.php
@@ -1086,40 +1086,40 @@ class B2bBotController extends \yii\web\Controller
 
 
     private function debug (){
-//        $orders = $this->getOrdersFromServer([
-//            'phone' => $this->dealer['phone'],
-//        ]);
-//
-//        Yii::info([
-//            'action'=>'response from Server - orders',
-//            'updateId'=>$this->request['update_id'],
-//            'serverResponse'=>$orders,
-//        ], 'b2bBot');
-//
-//        if (isset($orders['error'])) {
-//            return $this->sendErrorMessage('Ошибка - '.$orders['message']);
-//        }
-//
-//        $responseToUser = '';
-//
-//        foreach ($orders as $item) {
-//            $responseToUser .= $item['orderId']
-//                .' - '.$item['totalCost'].'р.'
-//                .PHP_EOL
-//                .$item['status']['status'].' | '
-//                .$item['status']['payment'].' | '
-//                .$item['status']['delivey']
-//                .PHP_EOL
-////                .'-------------------------'
-//                .PHP_EOL;
-//        }
+        $orders = $this->getOrdersFromServer([
+            'phone' => $this->dealer['phone'],
+        ]);
+
+        Yii::info([
+            'action'=>'response from Server - orders',
+            'updateId'=>$this->request['update_id'],
+            'serverResponse'=>$orders,
+        ], 'b2bBot');
+
+        if (isset($orders['error'])) {
+            return $this->sendErrorMessage('Ошибка - '.$orders['message']);
+        }
+
+        $responseToUser = '';
+
+        foreach ($orders as $item) {
+            $responseToUser .= $item['orderId']
+                .' - '.$item['totalCost'].'р.'
+                .PHP_EOL
+                .$item['status']['status'].' | '
+                .$item['status']['payment'].' | '
+                .$item['status']['delivey']
+                .PHP_EOL
+//                .'-------------------------'
+                .PHP_EOL;
+        }
 
 
 
 
         $options = [
             'chat_id' => $this->user['telegram_user_id'],
-            'text' => 'чек чек ',
+            'text' => $responseToUser,
             'reply_markup' => Json::encode([
                 'inline_keyboard'=>[
                     [
