b72d1dedb52fedeb103617152d21bf218e66bc49
Quadrosh <quadrosh@gmail.com>
2018-03-29 12:06:21 +0300

    yii queue 0.7

--- yapp/common/jobs/YiiJob.php
+++ yapp/common/jobs/YiiJob.php
@@ -9,8 +9,8 @@ use Yii;
 /**
  * Class YiiJob.
  */
-//class YiiJob extends \yii\base\Object implements \yii\queue\RetryableJob
-class YiiJob extends \yii\base\Object implements \yii\queue\Job
+class YiiJob extends \yii\base\Object implements \yii\queue\RetryableJob
+//class YiiJob extends \yii\base\Object implements \yii\queue\Job
 {
     public $options;
 
