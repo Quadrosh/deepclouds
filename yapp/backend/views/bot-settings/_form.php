170908693694f0988320fc76f5ae20ff34806e93
Quadrosh <quadrosh@gmail.com>
2018-04-05 18:39:55 +0300

    settings 0.11

--- yapp/backend/views/bot-settings/_form.php
+++ yapp/backend/views/bot-settings/_form.php
@@ -23,13 +23,13 @@ use yii\widgets\ActiveForm;
             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-sm-6">
-            <?= $form->field($model, 'value')->textarea(['rows' => 1]) ?>
+            <?= $form->field($model, 'discription')->textInput(['maxlength' => true]) ?>
         </div>
     </div>
 
     <div class="row">
         <div class="col-sm-12">
-            <?= $form->field($model, 'discription')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'value')->textarea(['rows' => 1]) ?>
         </div>
     </div>
 
