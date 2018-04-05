602abcb44b58f17a523bab0add78962bff8b70f8
Quadrosh <quadrosh@gmail.com>
2018-04-05 17:12:09 +0300

    settings 0.5

--- yapp/frontend/controllers/B2bBotController.php
+++ yapp/frontend/controllers/B2bBotController.php
@@ -1193,7 +1193,7 @@ class B2bBotController extends \yii\web\Controller
 
         $this->settings = BotSettings::find()
             ->where(['bot_name'=>'b2b'])
-            ->indexBy(['bot_name'])
+            ->indexBy('bot_name')
             ->asArray()
             ->all();
         return $this->settings;
