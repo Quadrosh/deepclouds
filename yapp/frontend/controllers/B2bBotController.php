78e1a4016223980361dbc3e55f374da933d7385f
Quadrosh <quadrosh@gmail.com>
2018-04-06 22:52:51 +0300

    job result 0.13

--- yapp/frontend/controllers/B2bBotController.php
+++ yapp/frontend/controllers/B2bBotController.php
@@ -1130,19 +1130,19 @@ class B2bBotController extends \yii\web\Controller
                 return 'ok';
             }
             // Other processing
-//            $this->sendMessage([
-//                'chat_id' => $this->user['telegram_user_id'],
-//                'text' => 'перед таймаутом',
-//            ]);
-//            sleep(15);
+            $this->sendMessage([
+                'chat_id' => $this->user['telegram_user_id'],
+                'text' => 'перед таймаутом',
+            ]);
+            sleep(15);
 
         }
 
-        $this->sendMessage([
-            'chat_id' => $this->user['telegram_user_id'],
-            'text' => 'перед таймаутом',
-        ]);
-        sleep(15);
+//        $this->sendMessage([
+//            'chat_id' => $this->user['telegram_user_id'],
+//            'text' => 'перед таймаутом',
+//        ]);
+//        sleep(15);
 
 
 
