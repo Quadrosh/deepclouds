4edea47b793301e3d1b9851c5b5c0b7fa4c0c8cf
Quadrosh <quadrosh@gmail.com>
2018-04-06 22:33:26 +0300

    job result 0.05

--- yapp/frontend/controllers/B2bBotController.php
+++ yapp/frontend/controllers/B2bBotController.php
@@ -1111,10 +1111,28 @@ class B2bBotController extends \yii\web\Controller
     private function debug (){
 
 
-        $this->sendMessage([
-            'chat_id' => $this->user['telegram_user_id'],
-            'text' => $this->settings['m_search_init_1']['value'],
-        ]);
+        while(true) {
+            $start_time = time();
+            if ((time() - $start_time) > 10) {
+//                $serverError = [];
+//                $serverError['error'] = 1;
+//                $serverError['message'] = 'Извините, B2B сервер не отвечает'
+//                    .PHP_EOL .'В данный момент запрос не может быть обработан';
+//                $serverError['code'] = 500;
+//                return Json::encode($serverError);
+                $this->sendMessage([
+                    'chat_id' => $this->user['telegram_user_id'],
+                    'text' => 'выскочили по таймауту',
+                ]);
+            }
+            // Other processing
+
+        }
+
+        sleep(15);
+
+
+
 
 
 
