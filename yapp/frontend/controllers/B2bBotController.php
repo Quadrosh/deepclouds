eb47b29ff54abc530dbb1a00e44c075d8df4eba0
Quadrosh <quadrosh@gmail.com>
2018-04-06 22:48:30 +0300

    job result 0.11

--- yapp/frontend/controllers/B2bBotController.php
+++ yapp/frontend/controllers/B2bBotController.php
@@ -1110,42 +1110,40 @@ class B2bBotController extends \yii\web\Controller
 
     private function debug (){
 
-//        $this->sendMessage([
-//            'chat_id' => $this->user['telegram_user_id'],
-//            'text' => 'вошли в debug',
-//        ]);
 
 
-//        while(true) {
-//            $start_time = time();
-//            if ((time() - $start_time) > 10) {
-////                $serverError = [];
-////                $serverError['error'] = 1;
-////                $serverError['message'] = 'Извините, B2B сервер не отвечает'
-////                    .PHP_EOL .'В данный момент запрос не может быть обработан';
-////                $serverError['code'] = 500;
-////                return Json::encode($serverError);
-//                $this->sendMessage([
-//                    'chat_id' => $this->user['telegram_user_id'],
-//                    'text' => 'выскочили по таймауту',
-//                ]);
-//                return 'ok';
-//            }
-//            // Other processing
-//            $this->sendMessage([
-//                'chat_id' => $this->user['telegram_user_id'],
-//                'text' => 'перед таймаутом',
-//            ]);
-//            sleep(15);
-//
-//        }
+        $start_time = time();
+
+        while(true) {
+
+            if ((time() - $start_time) > 10) {
+//                $serverError = [];
+//                $serverError['error'] = 1;
+//                $serverError['message'] = 'Извините, B2B сервер не отвечает'
+//                    .PHP_EOL .'В данный момент запрос не может быть обработан';
+//                $serverError['code'] = 500;
+//                return Json::encode($serverError);
+                $this->sendMessage([
+                    'chat_id' => $this->user['telegram_user_id'],
+                    'text' => 'выскочили по таймауту',
+                ]);
+                return 'ok';
+            }
+            // Other processing
+            $this->sendMessage([
+                'chat_id' => $this->user['telegram_user_id'],
+                'text' => 'перед таймаутом',
+            ]);
+            sleep(15);
+
+        }
 
 
 
-//        $this->sendMessage([
-//            'chat_id' => $this->user['telegram_user_id'],
-//            'text' => 'после таймаута',
-//        ]);
+        $this->sendMessage([
+            'chat_id' => $this->user['telegram_user_id'],
+            'text' => 'после таймаута',
+        ]);
 
 
 
